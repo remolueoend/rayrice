3adcc8706635c63c322b2aa4adfa8e515c97d227
Remo Zumsteg <remolueoend@users.noreply.github.com>
2020-02-29 14:45:51 +0100

    status bar updates, new i3 bindings for music/chat workspaces

---
+++ .local/bin/workspaces/ws-open-chat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+i3-msg 'workspace 11:Chat; exec --no-startup-id discord; workspace 11:Chat; exec --no-startup-id whatsapp-nativefier'
---
+++ .local/bin/workspaces/ws-open-music.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+i3-msg 'workspace 12:Music; exec spotify'
