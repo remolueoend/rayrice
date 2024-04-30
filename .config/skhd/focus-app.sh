2e329d32cd9f3e1a5802ed0e1a4d75f26e76d05b
remolueoend <remolueoend@users.noreply.github.com>
2024-05-01 00:42:35 +0200

    add configs for skhd and yabai

---
+++ .config/skhd/focus-app.sh
@@ -0,0 +1,1 @@
+osascript -e "tell application \"$1\" to activate"
---
+++ .config/skhd/open-terminal.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+osascript <<EOF
+on is_running(appName)
+    tell application "System Events" to (name of processes) contains appName
+end is_running
+
+if not is_running("iTerm") then
+  tell application "iTerm" to activate
+end if
+
+tell application "iTerm"
+    create window with default profile
+end tell
+
+EOF
